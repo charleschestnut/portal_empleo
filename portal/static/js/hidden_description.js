56034c049be03b71ee47694c4901923d613b2c47
charleschestnut <carloscastcast97@gmail.com>
2020-03-28 14:53:05 +0100

    feat: finally all static is together. Deleted static extra folder

--- portal/static/js/hidden_description.js
+++
@@ -1,8 +0,0 @@
-function hide_description() {
-  var x = document.getElementByClassName("hidden_description");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
